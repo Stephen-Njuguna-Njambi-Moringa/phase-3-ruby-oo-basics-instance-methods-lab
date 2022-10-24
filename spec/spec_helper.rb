b39b223d3dbc3a9a83dabfd4b0c578047462ad3c
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-10-24 11:55:00 +0300

    Completed Lab

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,31 @@
+require "pry"
 require_relative '../lib/dog'
 require_relative '../lib/person'
 
 RSpec.configure do |config|
   config.order = :default
 end
+
+class Dog
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+
+# binding.pry
